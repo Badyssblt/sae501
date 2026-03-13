b96f25329d538bb28abb2a63af8037397218236a
Badyss Blilita <badyss.blt@gmail.com>
2026-03-13 11:41:36 +0100

    'fix

--- Assets/Scripts/Counter/TrashBin.cs
+++ Assets/Scripts/Counter/TrashBin.cs
@@ -5,6 +5,17 @@ public class TrashBin : MonoBehaviour, IInteractable
     [SerializeField] private AudioClip trashSound;
     [SerializeField] private SpriteRenderer trashSprite;
 
+    private void Awake()
+    {
+        // S'assurer que la poubelle s'affiche au-dessus du tilemap (sortingOrder 1)
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr != null)
+        {
+            sr.sortingLayerName = "Main";
+            sr.sortingOrder = 2;
+        }
+    }
+
     public void Interact(PlayerInteraction player)
     {
         InventorySystem playerInventory = player.GetComponent<InventorySystem>();
